e71e4a47536c10b797a80ccaa58e67262785cb97
PioKl <kosiu342@gmail.com>
2019-12-23 18:37:15 +0100

    Updates

---
+++ precache-manifest.eaf67376def0c7fc4299e6a99235e6d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86c3ee6ba62b4da6697e",
+    "url": "/DrinkSearchApp/static/css/main.5d3f4e8b.chunk.css"
+  },
+  {
+    "revision": "86c3ee6ba62b4da6697e",
+    "url": "/DrinkSearchApp/static/js/main.0d22457e.chunk.js"
+  },
+  {
+    "revision": "2ab61317feb4b8fb91b6",
+    "url": "/DrinkSearchApp/static/js/runtime~main.b90ffffc.js"
+  },
+  {
+    "revision": "4bc0d5877704551d1358",
+    "url": "/DrinkSearchApp/static/js/2.5eea81e8.chunk.js"
+  },
+  {
+    "revision": "142f29b17fdeda20d020d6c74d378e7c",
+    "url": "/DrinkSearchApp/static/media/drinkArt2.142f29b1.png"
+  },
+  {
+    "revision": "49330763598499b0c296aa515dc2d903",
+    "url": "/DrinkSearchApp/index.html"
+  }
+];
